e27d3384f7249fdc199beae1dfa899e08a8e8954
felixblochwitz <felix.blochwitz@protonmail.com>
2025-02-22 23:24:22 +0100

    add basic functionality to show books on page

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const myLibrary = [];
-
-console.log(myLibrary);
+const bookDivs = [];
+const bookDetails = document.getElementById("book-details");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const readStatus = document.getElementById("readStatus");
 
 function Book(title, author, publisher, pages, language, readStatus) {
   this.title = title;
@@ -24,20 +28,20 @@ function addBookToLibrary(
 }
 
 addBookToLibrary(
-  "Before They Are Hanged",
+  "The Blade itself",
   "Joe Abercrombie",
   "Orion",
-  570,
+  536,
   "English",
   true,
   myLibrary,
 );
 
 addBookToLibrary(
-  "The Blade itself",
+  "Before They Are Hanged",
   "Joe Abercrombie",
   "Orion",
-  536,
+  570,
   "English",
   true,
   myLibrary,
@@ -53,17 +57,27 @@ addBookToLibrary(
   myLibrary,
 );
 
-//console.log(myLibrary);
-
 function showBooksOnPage(lib) {
   const bookshelf = document.querySelector(".bookshelf");
   console.log(bookshelf);
   lib.forEach((book) => {
     const bookDiv = document.createElement("div");
     bookDiv.className = "book";
-    bookDiv.textContent = book.title
-    bookshelf.appendChild(bookDiv)
+    bookDiv.textContent = book.title;
+    bookshelf.appendChild(bookDiv);
+    bookDivs.push(bookDiv);
   });
 }
 
 showBooksOnPage(myLibrary);
+
+bookDivs.forEach((x, i) => {
+  const book = myLibrary[i];
+  x.addEventListener("click", () => {
+    title.textContent = `Title: ${book.title}`;
+    author.textContent = `Author: ${book.author}`;
+    pages.textContent = `Pages: ${book.pages}`;
+    readStatus.textContent = `Already read: ${book.readStatus ? "\u2705" : "\u274c"}`;
+    bookDetails.showModal();
+  });
+});
